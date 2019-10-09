bffce0242ce6b5b347d1272d283cb123c3751780
josefinecolbergjensen <Joje19ae@student.cbs.dk>
2019-10-09 16:08:59 +0200

    Kommentarer til student

--- Javascript/student.js
+++ Javascript/student.js
@@ -1,12 +1,12 @@
 class student {
-    constructor(studyProgram, creditCardInfo, courses, semester) {
+    constructor(studyProgram, creditCardInfo, courses, semester) {  //Ekstra attributter som er opstillet til student
         this.studyProgram = studyProgram;
         this.creditCardInfo = creditCardInfo;
         this.courses = courses;
         this.semester = semester;
     }
 }
-    student.prototype = new user();
+    student.prototype = new user();              //Vi kalder user som har vores attributter der er gældende for alle grupper.
 student.prototype.constructor = student;
 
 

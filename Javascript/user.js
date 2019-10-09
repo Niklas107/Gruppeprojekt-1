2fa4f0ed8887ceaf406fc5cae9d8dec46b6e1745
Niklas107 <54836150+Niklas107@users.noreply.github.com>
2019-10-09 15:35:48 +0200

    Update user.js

--- Javascript/user.js
+++ Javascript/user.js
@@ -1,1 +1,10 @@
-class user {}
+class user {
+    constructor(userName, password, email, phoneNumber, firstName, lastName) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.firstName= firstName;
+        this.lastName = lastName;
+    }
+}

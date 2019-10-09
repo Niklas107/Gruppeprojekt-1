a8823689f6811da03bf5e7479cc65276fd794db6
josefinecolbergjensen <Joje19ae@student.cbs.dk>
2019-10-09 16:18:02 +0200

    Kommentarer til admin

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -1,8 +1,8 @@
 class admin {
-    constructor(adminName) {
+    constructor(adminName) {             //Ekstra attributter som er opstillet til student
         this.adminName = adminName;
     }
 }
-admin.prototype = new user();
+admin.prototype = new user();          ////Vi kalder user som har vores attributter der er gældende for alle grupper.
 admin.prototype.constructor = admin;
 

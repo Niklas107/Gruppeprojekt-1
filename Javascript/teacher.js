209b4134cf378d5c8d9dd8805d382ea9bd535d18
[nilb1] <[nily19ab@student.cbs.dk]>
2019-10-09 16:17:58 +0200

    Jeg har tilføjet kommentarer til Teacher

--- Javascript/teacher.js
+++ Javascript/teacher.js
@@ -1,8 +1,8 @@
 class teacher {
-    constructor(studyProgram) {
+    constructor(studyProgram) {     //Ekstra attributter som er opstillet til teacher
         this.studyProgram = studyProgram;
     }
 }
-teacher.prototype = new user();
+teacher.prototype = new user();     //Vi kalder user som har vores attributter der er gældende for alle grupper.
 teacher.prototype.constructor = teacher;
 

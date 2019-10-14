17b0320e98438c9266d50f41bddb4b327962acd8
josefinecolbergjensen <Joje19ae@student.cbs.dk>
2019-10-14 09:37:03 +0200

    Funktion til admin

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -6,3 +6,13 @@ class admin {
 admin.prototype = new user();          ////Vi kalder user som har vores attributter der er gældende for alle grupper.
 admin.prototype.constructor = admin;
 
+class newTeacher {
+    constructor(firstName,lastName,email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
+
+const niller = newTeacher("Niklas","Niller","nik123@gmail.com");
+console.log(niller);

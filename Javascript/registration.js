d2a781a80a13a5a8a795e46e8bfcd675d6a52b5e
chris <chan19af@student.cbs.dk>
2019-10-15 11:46:21 +0200

    registration og fucket rundt med student

---
+++ Javascript/registration.js
@@ -0,0 +1,15 @@
+function Register() {
+    var name = document.getElementById("user_name").value;
+    var address = document.getElementById("address").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var comments = document.getElementById("comment").value;
+
+    alert("You have been registered"
+        + "\nName: " + name
+        + "\naddress: " + address
+        + "\nemail: " + email
+        + "\nphone: " + phone
+        + "\npassword: " + "********"
+        + "\nAdditional comment:" + " " + comments);
+}
--- Javascript/student.js
+++ Javascript/student.js
@@ -1,4 +1,4 @@
-class student extends user {                                                                                                    //Vi extender vores værdier fra User
+/*class student extends user {                                                                                                    //Vi extender vores værdier fra User
     constructor(userName, password, email, phoneNumber, firstName, lastName, studyProgram, course, semester) {
         super(userName, password, email, phoneNumber, firstName, lastName);                                                       //Vi bruger super-funktionen.
 
@@ -6,7 +6,16 @@ class student extends user {                                                    
         this.course = course;
         this.semester = semester;
     }
+}*/
+
+var programDropdown = document.getElementById("selectStudyProgram")
+programDropdown.onchange = function() {
+    if(programDropdown.value == "HA(it.)"){
+        console.log(document.getElementsByClassName("HA(it.)"))
+
+    }
 }
+
 function chooseCourse() {
     var studyprogram = document.getElementById("selectStudyProgram").value;
     var course = document.getElementById("studentCourses").value;
@@ -21,8 +30,8 @@ function chooseCourse() {
 }
 
 
-Niklas = new student("Niller", "12345", "Nik123@gmail.com", "12345678", "Niklas","jh",""," ",""  );
-console.log(Niklas);
+//Niklas = new student("Niller", "12345", "Nik123@gmail.com", "12345678", "Niklas","jh",""," ",""  );
+//console.log(Niklas);
 
 
 

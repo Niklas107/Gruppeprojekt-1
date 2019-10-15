3ef68fe901b4be95ad3602ba07d4343e75afe343
Alexa <alal19ag@student.cbs.dk>
2019-10-15 12:46:05 +0200

    Registration: FIXED for real now

--- Javascript/registration.js
+++ Javascript/registration.js
@@ -1,13 +1,13 @@
 function Register() {
-    var name = document.getElementById("user_name").value;
-    var address = document.getElementById("address").value;
+    var user_name = document.getElementById("user_name").value;
+    var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
     var comments = document.getElementById("comment").value;
 
     alert("You have been registered" +
-        "\nName: " + name +
-        "\naddress: " + address +
+        "\nuserName: " + user_name +
+        "\nname: " + name +
         "\nemail: " + email +
         "\nphone: " + phone +
         "\npassword: " + "****" +

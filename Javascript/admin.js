a027d906e432cc8969d18fc6c0db0ec3c54bebe8
josefinecolbergjensen <Joje19ae@student.cbs.dk>
2019-10-15 08:25:35 +0200

    Rettelser i admin.js

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -1,4 +1,4 @@
-class admin extends user{                                                                   // Vi extender fra user
+class admin extends user {                                                                   // Vi extender fra user
     constructor(userName, password, email, phoneNumber, firstName, lastName, adminName) {   // Alle attributter til admin
         super(userName, password, email, phoneNumber, firstName, lastName);
         this.adminName = adminName;                                                         //Ekstra attributter som er opstillet til admin

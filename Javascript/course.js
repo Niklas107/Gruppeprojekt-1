52dabeb9cad98c2d09c194fd07e04f3cb5a550ea
[nilb1] <[nily19ab@student.cbs.dk]>
2019-10-18 09:58:08 +0200

    vi kan ikke få det til at virke

--- Javascript/course.js
+++ Javascript/course.js
@@ -1,5 +1,8 @@
-class courses {
+class course {
     constructor(studyProgram, semester) {               //Attributter opstillet som courses skal bruge
         this.studyProgram = studyProgram;
         this.semester = semester;
 
+
+BIS = new course ("Ha(it.)", "1");
+console.log(BIS);

9ff7af5fa85436aea1da7e0b0c50fc2bbd5dfc4d
chris <chan19af@student.cbs.dk>
2019-10-18 12:35:04 +0200

    Har lavet validation form til regristration

--- Javascript/registration.js
+++ Javascript/registration.js
@@ -8,10 +8,7 @@
 
     var comment = document.getElementById("comment");
 
-    if(!userName  && !fullName && !email && !phone && !comment ){
-        alert("Enter at least one search parameter!");
-        return false;
-    }
+    var password = document.getElementById("password");
 
 //Different variables needed in our registration form
 
@@ -48,29 +45,66 @@ function storeLogin() {
     localStorage.setItem("comment", comment.value);
 
     console.log(fullName.value);
-
-    alert("You have been registered"
-
-        + "\nuserName: " + userName.value
-
-        + "\nname: " + fullName.value
-
-        + "\nemail: " + email.value
-
-        + "\nphone: " + phone.value
-
-        + "\npassword: " + "****"
-
-        + "\nAdditional comment: " + comment.value);
-
 }
 
 
-
+/*
 function showUser () {
 
     console.log("Show user stored in localStorage.")
 
     console.log(localStorage);
+*/
+//validation form
 
-}
+    function validateContactInformation() {
+        var form_valid = true;
+        var validation_message = "";
+
+        if (userName.value == null || userName.value == ""){
+            validation_message += "Username must be filled in! \n";
+            form_valid = false; }
+
+        if (fullName.value == null || fullName.value == ""){
+                validation_message += "fullName must be filled in! \n";
+                form_valid = false; }
+
+        var atpos = email.value.indexOf("@");
+        var dotpos = email.value.lastIndexOf(".");
+        if (atpos<1||dotpos < atpos + 2 || dotpos + 2 > email.lenght){
+            validation_message += "Please enter a valid email \n";
+            form_valid = false;}
+
+        if (isNaN(phone.value)){
+            validation_message += "Phonenumbers can only consist of numbers! \n";
+            form_valid = false;}
+        else if (phone.value == null || phone.value == "") {
+            validation_message += "Please enter a phone number \n";
+            form_valid = false ;}
+
+        if (password.value == null || password.value == ""){
+            validation_message += "Password must be filled in! \n";
+            form_valid = false; }
+
+        if (form_valid) {
+            alert("You have been registered"
+
+                + "\nuserName: " + userName.value
+
+                + "\nname: " + fullName.value
+
+                + "\nemail: " + email.value
+
+                + "\nphone: " + phone.value
+
+                + "\npassword: " + "****"
+
+                + "\nAdditional comment: " + comment.value);
+        }
+        else {
+            alert(validation_message);
+
+            }
+        return (form_valid);
+
+    }

265e673ced62c920a3eaba2e9dec0013c605105a
Alexa <alal19ag@student.cbs.dk>
2019-10-18 12:50:02 +0200

    Finpudsning af regist v2

--- Javascript/registration.js
+++ Javascript/registration.js
@@ -1,52 +1,29 @@
     var userName = document.getElementById("userName");
-
     var fullName = document.getElementById("fullName");
-
     var email = document.getElementById("email");
-
     var phone = document.getElementById("phone");
-
     var comment = document.getElementById("comment");
-
     var password = document.getElementById("password");
 
 //Different variables needed in our registration form
-
 function Register() {
-
     this.userName = document.getElementById("userName").value;
-
     this.name = document.getElementById("name").value;
-
     this.email = document.getElementById("email").value;
-
     this.phone = document.getElementById("phone").value;
-
     this.comment = document.getElementById("comment").value;
-
-
-
 }
-
-
 
 //Store input from registration to localStorage
 
 function storeLogin() {
-
     localStorage.setItem("userName", userName.value);
-
     localStorage.setItem("name", fullName.value);
-
     localStorage.setItem("email", email.value);
-
     localStorage.setItem("phone", phone.value);
-
     localStorage.setItem("comment", comment.value);
-
     console.log(fullName.value);
 }
-
 
 /*
 function showUser () {
@@ -56,7 +33,6 @@ function showUser () {
     console.log(localStorage);
 */
 //validation form
-
     function validateContactInformation() {
         var form_valid = true;
         var validation_message = "";
@@ -90,21 +66,14 @@ function showUser () {
             alert("You have been registered"
 
                 + "\nuserName: " + userName.value
-
                 + "\nname: " + fullName.value
-
                 + "\nemail: " + email.value
-
                 + "\nphone: " + phone.value
-
                 + "\npassword: " + "****"
-
                 + "\nAdditional comment: " + comment.value);
         }
         else {
             alert(validation_message);
-
             }
         return (form_valid);
-
     }

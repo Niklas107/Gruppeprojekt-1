51023f03c98006101d1303c9676d5003b78bc0d9
Alexa <alal19ag@student.cbs.dk>
2019-10-18 11:14:45 +0200

    Rettelse af syntaks. Mangler: skal have min. til at virke

--- Javascript/registration.js
+++ Javascript/registration.js
@@ -1,37 +1,73 @@
- var userName = document.getElementById("userName");
- var fullName = document.getElementById("fullName");
- var email = document.getElementById("email");
- var phone = document.getElementById("phone");
+var userName = document.getElementById("userName");
+
+var fullName = document.getElementById("fullName");
+
+var email = document.getElementById("email");
+
+var phone = document.getElementById("phone");
+
 var comment = document.getElementById("comment");
+if (userName>6 || fullName>6 || email>8 || phone>=8 || comment<300 )
+
 
 //Different variables needed in our registration form
+
 function Register() {
+
     this.userName = document.getElementById("userName").value;
+
     this.name = document.getElementById("name").value;
+
     this.email = document.getElementById("email").value;
+
     this.phone = document.getElementById("phone").value;
+
     this.comment = document.getElementById("comment").value;
 
+
+
 }
 
-//Store input from registration to localStorage.
+
+
+//Store input from registration to localStorage
+
 function storeLogin() {
-        localStorage.setItem("userName", userName.value);
-        localStorage.setItem("name", fullName.value);
-        localStorage.setItem("email", email.value);
-        localStorage.setItem("phone", phone.value);
-        localStorage.setItem("comment", comment.value);
-        console.log(fullName.value)
- alert("You have been registered"
+
+    localStorage.setItem("userName", userName.value);
+
+    localStorage.setItem("name", fullName.value);
+
+    localStorage.setItem("email", email.value);
+
+    localStorage.setItem("phone", phone.value);
+
+    localStorage.setItem("comment", comment.value);
+
+    console.log(fullName.value);
+
+    alert("You have been registered"
+
         + "\nuserName: " + userName.value
+
         + "\nname: " + fullName.value
+
         + "\nemail: " + email.value
+
         + "\nphone: " + phone.value
+
         + "\npassword: " + "****"
+
         + "\nAdditional comment: " + comment.value);
+
 }
+
+
 
 function showUser () {
+
     console.log("Show user stored in localStorage.")
+
     console.log(localStorage);
+
 }

d1e2aff29d9ec53d52d8c0bb02c0722e73361d73
josefinecolbergjensen <Joje19ae@student.cbs.dk>
2019-10-23 16:10:16 +0200

    Local storage i admin

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -1,9 +1,31 @@
 class Admin extends User {                                                                   // Vi extender fra user
-    constructor(userName, password, email, phoneNumber, firstName, lastName, adminName) {   // Alle attributter til admin
-        super(userName, password, email, phoneNumber, firstName, lastName);
-        this.adminName = adminName;                                                         //Ekstra attributter som er opstillet til admin
+    constructor(userName, password, email, phoneNumber, firstName, lastName) {   // Alle attributter til admin
     }
     }
+
+
+//Local Storage in order to make a admin able to log in.
+console.log(localStorage.getItem("Admin"))
+var adminList = []
+
+if (localStorage.getItem("Admin") == null) {
+
+    adminList.push(new Admin("","","","","","","");
+    var adminListString = JSON.stringify(adminList)
+    localStorage.setItem('Admin', adminListString)
+
+} else {
+    adminList = JSON.parse(localStorage.getItem('Admin'))
+}
+
+
+
+
+
+
+
+
+
 
 //Lav en removeTeacher og removeStudent funktion
 

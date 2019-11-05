4a092e43f87955c28ee17f9f56a61eb65e6a5c7e
[nilb1] <nily19ab@student.cbs.dk>
2019-11-05 11:13:53 +0100

    Course bruges ikke, og kommentarer tilføjet til admin og classroom

--- Javascript/classroom.js
+++ Javascript/classroom.js
@@ -1,17 +1,21 @@
+// Klassenn Classroom er her opstillet med nogle attributter - Nik
 class Classroom {
-    constructor(size, location) {               //Attributter opstillet som courses skal bruge
+    constructor(size, location) {
         this.size = size;
         this.location = location;
     }
 }
+// Her er der hardcodet nogle classrooms som bruges i createLecture funktionen - Nik
 room1 = new Classroom("50", "Solbjerg Plads");
 room2 = new Classroom("40", "Dalgas Have");
 room3 = new Classroom("120", "Solbjerg Plads");
 room4 = new Classroom("90", "Kilen");
 room5 = new Classroom("50", "Flintholm");
+/* Det her skal opdateres, men skulle gerne gøre de hardcodede classrooms til strings, så de kan være med i alerten når man opretter en forelæsning.
+Hvis det kommer til at virke, skal det indgå i createLecture funktionen i stedet for at console.log dem - Nik */
 var test1 = JSON.stringify(room1);
 
-
+// Denne funktion console.logger det valgte classroom, så når man opretter en forelæsning, kan man se informationerne om det valgte classroom - Nik
 function getSelectClassroom() {
     var selectedClassroom = document.getElementById("Classroom").value;
     if (selectedClassroom === "room1") {

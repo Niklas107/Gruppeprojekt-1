953fe95b2a2bcde494e1e7f2cf35443d6ca84e16
Niklas107 <nily19ab@student.cbs.dk>
2019-12-03 11:28:55 +0100

    Create lecture virker igen venner brr brr - Nik

--- Javascript/teacher.js
+++ Javascript/teacher.js
@@ -147,7 +147,7 @@ function createLecture() {
     var teacher1 = document.getElementById("teacher1").value;
     var studyProgram1 = document.getElementById("studyProgram").value;
     var course1 = document.getElementById("course").value;
-    var classRoom1 = document.getElementById("Classroom").value;
+    var classRoom1 = document.getElementById("chooseClassroom").value;
     var lectureName1 = document.getElementById("lectureName").value;
     var day1 = document.getElementById("day").value;
     var time1 = document.getElementById("time").value;
@@ -157,7 +157,7 @@ function createLecture() {
         + "\nTeacher: " + teacher1
         + "\nStudy program: " + studyProgram1
         + "\nCourse: " + course1
-        + "\nClassroom: " + classRoom1 + " - " + test1   //test1 skal slettes?? gør at det ikke virker???
+        + "\nClassroom: " + classRoom1
         + "\nLecture name: " + lectureName1
         + "\nDay: " + day1
         + "\nTime of lecture: " + time1
